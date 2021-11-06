b9e0312872a9cc600a743444b58b17d380f2ad6e
`Thomas <`pontom@gmx.at>
2021-11-06 23:07:25 +0100

    Multiplikation mit Variablen b2,b3 aus Liste

--- src/Bruch.java
+++ src/Bruch.java
@@ -27,6 +27,7 @@ public class Bruch {
     public String print() {
         // TODO: Die Methode soll den Bruch als Text retournieren z.B.: "5 / 7"
         return numerator+"/"+denominator;
+        //gibt als Bruch retour
     }
 
     public Bruch multiplicate(Bruch b2) {
@@ -39,12 +40,14 @@ public class Bruch {
         * */
 
 
-        return null;
+        return new Bruch(numerator* b2.getNumerator(),denominator* b2.getDenominator());
+        //Multiplikation mit Variablen b2 aus Liste
     }
 
     public Bruch multiplicate(Bruch b2,  Bruch b3) {
         // TODO: Multiplizieren Sie den eigenen Bruch mit b2 und b3
 
-        return null;
+        return new Bruch( numerator * b2.getNumerator()* b3.getNumerator(), denominator* b2.getDenominator()* b3.getDenominator());
+        //Multiplikation mit Variablen b2,b3 aus Liste
     }
 }
